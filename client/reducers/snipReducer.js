0238f48fb7b0d8f8343f44e5fe7efd3d76c2904e
Shane Taylor <shaneallantaylor@gmail.com>
2019-03-14 14:30:09 -0700

    fixed merge error with snipReducer

--- client/reducers/snipReducer.js
+++ client/reducers/snipReducer.js
@@ -1,28 +1,24 @@
 import * as types from '../constants/actionTypes';
 
 const initialState = {
-	snippet: '',
-	comments: '',
-	project: '',
-	tags: '',
-	search: '',
-	recievedTags: [],
-	recievedSnippet: '',
+  snippet: '',
+  comments: '',
+  project: '',
+  tags: '',
+  search: '',
+  recievedTags: [],
+  recievedSnippet: '',
 
 }
 
 const snipReducer = (state = initialState, action) => {
   switch (action.type) {
 
-<<<<<<< HEAD
-  	case types.GET_SNIPPETS:
-=======
-  	case types.GET_SNIPPET_BY_TAG:
->>>>>>> 4fb54fe6cf3fdce4de56e23b66942594e17137d4
-  	  return {
-  	  	...state,
-  	    recievedSnippet: action.payload,
-  	  }
+    case types.GET_SNIPPET_BY_TAG:
+      return {
+        ...state,
+        recievedSnippet: action.payload,
+      }
 
     case types.GET_SNIPPET_BY_USER:
       return {
@@ -30,13 +26,13 @@ const snipReducer = (state = initialState, action) => {
         recievedSnippet: action.payload,
       }
 
-  	case types.GET_TAGS:
-  	  return {
-  	  	...state,
-  	    recievedTags: action.payload,
-  	  }
+    case types.GET_TAGS:
+      return {
+        ...state,
+        recievedTags: action.payload,
+      }
 
-  	case types.ENTER_SNIPPET:
+    case types.ENTER_SNIPPET:
       return {
         ...state,
         snippet: action.payload,
